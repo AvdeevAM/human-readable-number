2091f09365955a20c9228dbe5b27b0cd4b386ef1
avdeevam <avdeev.am471@gmail.com>
2021-03-12 18:37:52 +0300

    feat: add numanReadableNumber

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable (s){
+    var dg = ['zero','one','two','three','four','five','six','seven','eight','nine']; 
+    var teen = ['ten','eleven','twelve','thirteen', 'fourteen','fifteen','sixteen','seventeen','eighteen','nineteen'];
+    var ty = ['ten','twenty','thirty','forty','fifty','sixty','seventy','eighty','ninety'];
+    if (s < 10){
+        return dg[s];
+    }
+    if (s >= 10 && s < 20){
+        return teen[s - 10];
+    }
+    str = s.toString();
+    var n = str.split('').length;
+    if (n == 2){
+        if (s % 10 == 0){
+            return ty[s / 10 - 1];
+        }
+        return ty[str.charAt(0) - 1] + ' ' + dg[str.charAt(1)];
+    }
+    if (n == 3){
+        if (s % 100 == 0){
+            return dg[s / 100] + ' hundred';
+        }
+        if (s % 10 == 0){
+            return dg[Math.floor(s / 100)] + ' hundred ' + ty[(s % 100) / 10 - 1];
+        }
+        if (s % 100 < 10){
+            return dg[Math.floor(s / 100)] + ' hundred ' + dg[s % 100];
+        }
+        if (s % 100 < 20){
+            return dg[Math.floor(s / 100)] + ' hundred ' + teen[s % 100 - 10];
+        }
+        return  dg[str.charAt(0)] + ' hundred ' + ty[str.charAt(1) - 1] + ' ' + dg[str.charAt(2)];
+    }
 }
+  
+
